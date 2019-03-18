eecc824f4436145abac7788cdbba497f63c36cd7
Remi GASCOU <remi@gascou.net>
2019-03-18 19:47:21 +0100

    .

--- C/buffertools/livedump.sh
+++ C/buffertools/livedump.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-while true; do clear; ./dump ; sleep 0.5; done
+DUMP_EXEC=./dump
+if [ -f ${DUMP_EXEC} ]; then
+    while true; do clear; ${DUMP_EXEC} ; sleep 0.5; done
+else
+    echo -e "\x1b[1m[\x1b[91mwarn\x1b[0m\x1b[1m]\x1b[0m ${DUMP_EXEC} not found. Try \"make\"."
+fi
