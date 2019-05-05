b81a572a6988cbfb5fe4c6e02e6fcad3f02800a3
Remi GASCOU <remi@gascou.net>
2019-05-05 12:15:40 +0200

    Created PNGParser

---
+++ Python/PNG_Parser/lib/chunks/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : __init__
+# Author             : Remi GASCOU
+# Date created       : 05/05/2019
+# Date last modified : 05/05/2019
+# Python Version     : 3.*
+
+from lib.chunks.IHDR import *
+from lib.chunks.IDAT import *
+from lib.chunks.IEND import *
---
+++ Python/PNG_Parser/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : PNGParser
+# Author             : Remi GASCOU
+# Date created       :
+# Date last modified :
+# Python Version     : 3.*
+
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage : python3 "+sys.argv[0]+" image.png")
+    else :
+        image = sys.argv[1]
